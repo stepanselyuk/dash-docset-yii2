c851fe27134edc4647795c5c0eb35a95e4f74a56
Stephen Seliuk <stephen@seliuk.com>
2014-07-07 15:34:01 +0300

    Change the $phpexec default value in the config.sh

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 
 # you can change "php54" to "php" or other, which you have
 # check "whereis php"
-phpexec="/opt/local/bin/php54"
+phpexec="php"
 docset="yii2.docset/Contents/Resources/Documents/"
 
 runpath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
