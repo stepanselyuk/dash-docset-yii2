09c3e3626f28522055e2632191821a83dc32701d
Stephen Seliuk <stephen@seliuk.com>
2014-07-22 14:37:40 +0300

    Updated generating script. Renamed docset to Yii2.docset. Added tar-archive creating for Dash contribute. Added icon@2x.png for Retina displays.

--- config.sh
+++ config.sh
@@ -3,7 +3,7 @@
 # you can change "php54" to "php" or other, which you have
 # check "whereis php"
 phpexec="php"
-docset="yii2.docset/Contents/Resources/Documents/"
+docset="Yii2.docset/Contents/Resources/Documents/"
 
 runpath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo "Run path: $runpath"
